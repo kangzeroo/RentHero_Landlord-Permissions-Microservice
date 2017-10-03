5d772796c69407fe9b228f3a64cfdc5a7b59007c
Kangze Huang <kangzeroo@gmail.com>
2017-10-03 15:50:52 -0400

    prod on docker

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -it -p 3003:3003 --name=landlord_permissions_microservice landlord_permissions_microservice npm run dev -- --host=0.0.0.0
+docker run -d -it -p 3003:3003 --name=landlord_permissions_microservice landlord_permissions_microservice npm run prod -- --host=0.0.0.0
