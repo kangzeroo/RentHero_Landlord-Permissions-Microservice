c97ce835eff3734485ebe16c58ad01f670c18538
Jimmy <j63guo@uwaterloo.ca>
2018-01-21 10:33:22 -0500

    asd

--- Postgres/Queries/EmployeeQuery.js
+++ Postgres/Queries/EmployeeQuery.js
@@ -2,6 +2,7 @@ const Promise = require('bluebird')
 const { promisify } = Promise
 const pool = require('../db_connect')
 const uuid = require('uuid')
+const sendEmployeeMappedEmail = require('../../aws/ses/send_employee_mapping').sendEmployeeMappedEmail
 
 // to run a query we just pass it to the pool
 // after we're done nothing has to be taken care of
@@ -33,10 +34,10 @@ exports.insert_employee_mapping = (req, res, next) => {
     if (data && data.rows && data.rows.length > 0) {
       employee_id = data.rows[0].employee_id
     }
-    const values = [employee_id, info.corporation_id, info.email, info.phone, info.alias_email]
+    const values = [employee_id, info.corporation_id, info.corporateEmployee.name, info.corporateEmployee.email, info.corporateEmployee.phone, info.corporateEmployee.alias_email]
 
-    const insert_employee = `INSERT INTO employee (employee_id, corporation_id, email, phone, alias_email)
-                                  VALUES ($1, $2, $3, $4, $5)
+    const insert_employee = `INSERT INTO employee (employee_id, corporation_id, name, email, phone, alias_email)
+                                  VALUES ($1, $2, $3, $4, $5, $6)
                                 ON CONFLICT (email, phone) DO NOTHING
                             `
     return query(insert_employee, values)
@@ -50,6 +51,23 @@ exports.insert_employee_mapping = (req, res, next) => {
     return query(query_string, values2)
   })
   .then(() => {
+    const values3 = [info.corporation_id]
+    const get_corp_email = `SELECT email FROM corporation WHERE corporation_id = $1`
+
+    return query(get_corp_email, values3)
+  })
+  .then((data) => {
+    const corp_email = data.rows[0].email
+
+    return sendEmployeeMappedEmail({
+      email: corp_email,
+      employee: info.corporateEmployee,
+      tenant: info.tenant,
+      building: info.building,
+      inquiry: info.inquiry
+    })
+  })
+  .then(() => {
     res.json({
       message: 'Successfully Inserted Employee Mappings'
     })
@@ -58,5 +76,11 @@ exports.insert_employee_mapping = (req, res, next) => {
     console.log(err)
     res.status(500).send('Failed to insert employee mapping')
   })
+}
 
+exports.get_employees_for_inquiry = (req, res, next) => {
+  const info = req.body
+  const values = [info.inquiry_id]
+
+  const get_employees = `SELECT * FROM employees WHERE inquiry_id = $1`
 }
